0c8d3ac4df4476b0d01395cdd2ac43fb0a93e200
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2025-12-22 14:06:15 +0200

    refactor: Adjust password validation minimum length from 6 to 4 characters for improved user flexibility in authentication.

--- src/validations/auth.validations.tsx
+++ src/validations/auth.validations.tsx
@@ -9,8 +9,8 @@ export const validationSchema = Joi.object({
       'string.empty': 'Email is required',
       'string.email': 'Invalid email address',
     }),
-  password: Joi.string().min(6).required().messages({
+  password: Joi.string().min(4).required().messages({
     'string.empty': 'Password is required',
-    'string.min': 'Password must be at least 6 characters',
+    'string.min': 'Password must be at least 4 characters',
   }),
 });
